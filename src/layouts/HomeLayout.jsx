4328e2d0ac6a87260a7cb01157a72442a62584eb
junaidjesan2 <junaidjesan2@gmail.com>
2024-04-04 09:55:10 +0600

    initial folder structure created

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,6 @@
+
+export default function HomeLayout() {
+  return (
+    <div>HomeLayout</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Route from "./routes/Route.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={Route}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);

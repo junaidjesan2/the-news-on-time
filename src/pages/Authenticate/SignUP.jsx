af551b78e76cf81586081c663fd56fd1619a0257
junaidjesan2 <junaidjesan2@gmail.com>
2024-04-05 12:50:21 +0600

    sign in and sign up form created

--- src/pages/Authenticate/SignUP.jsx
+++ src/pages/Authenticate/SignUP.jsx
@@ -1,8 +1,36 @@
-
 function SignUP() {
   return (
-    <div>SignUP</div>
-  )
+    <div>
+      <div>
+        <div className="w-3/5 mx-auto px-10 py-14 bg-neutral-400 rounded-lg">
+          <h1 className="text-3xl text-center font-bold">SignIn Here</h1>
+          <form action="" className="w-1/2 mx-auto">
+            <div className="py-2 flex flex-col">
+              <label htmlFor="email" className="font-semibold">
+                Enter your Email
+              </label>
+              <input type="email" id="email" className="rounded-md" />
+            </div>
+            <div className="py-2 flex flex-col">
+              <label htmlFor="password" className="font-semibold">
+                Enter your Password
+              </label>
+              <input type="password" id="password" className="rounded-md" />
+            </div>
+            <div className="py-2 flex flex-col">
+              <label htmlFor="rePass" className="font-semibold">
+                Retype your Password again
+              </label>
+              <input type="rePass" id="rePass" className="rounded-md" />
+            </div>
+            <button className="bg-zinc-700 text-center py-2 w-1/2 mx-auto rounded-xl text-white font-semibold hover:bg-zinc-800 hover:rounded-2xl">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignUP
+export default SignUP;

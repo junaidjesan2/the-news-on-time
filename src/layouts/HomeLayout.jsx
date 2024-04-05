90df0cc482ae0fe9ecf3aa83c2dfd1e966f1210a
junaidjesan2 <junaidjesan2@gmail.com>
2024-04-05 11:53:35 +0600

    header section created and react fast marquee installed

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,15 +1,32 @@
 import { Outlet } from "react-router-dom";
+import Marquee from "react-fast-marquee";
+
 import Header from "../components/shared/Header";
+import logo from "../assets/logo.png";
 
 export default function HomeLayout() {
   return (
     <div>
-        <div>
-            <Header></Header>
+      <div>
+        <img src={logo} alt="" className="mx-auto mt-12" />
+        <p className="text-slate-400 mx-auto w-fit mt-3">
+          Journalism Without Fear or Favour
+        </p>
+        <div className="w-5/6 mx-auto bg-[#F3F3F3] py-5 rounded-md text-black">
+          <Marquee>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem, sed?
+            </p>
+          </Marquee>
         </div>
-        <div>
-            <Outlet></Outlet>
-        </div>
+      </div>
+      <div>
+        <Header></Header>
+      </div>
+      <div>
+        <Outlet></Outlet>
+      </div>
     </div>
-  )
+  );
 }
